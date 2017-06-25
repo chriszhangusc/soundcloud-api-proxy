910409b3775f0799f439267efb7fd8650f658667
Chris Zhang <chaoye917@gmail.com>
2017-06-24 20:06:21 -0700

    Update access origins

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use((req, res, next) => {
     'http://localhost:3000',
     'http://127.0.0.1:8080',
     'http://localhost:8080',
+    'https://soundnode-redux.herokuapp.com',
     'https://redux-music.herokuapp.com',
   ];
   const origin = req.headers.origin;
@@ -51,12 +52,12 @@ const proxyV2 = proxy(optionsV2);
 app.use('/sc/v1', proxyV1);
 app.use('/sc/v2', proxyV2);
 
-function logger(req, res, next) {
-  console.log(new Date(), req.method, req.url);
-  next();
-}
+// function logger(req, res, next) {
+//   console.log(new Date(), req.method, req.url);
+//   next();
+// }
 
-app.use(logger);
+// app.use(logger);
 app.listen(port, () => {
   console.log('Server running at port:', port);
 });
